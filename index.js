39ba0daef0f6eb3e6030fce587afb4e4c3a76118
Shareif Hall <shareifhall@yahoo.com>
2023-02-22 12:17:57 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,61 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(sumValue) {
+    if(sumValue > 42) {
+    return  sumValue - 42;
+    }
+    else {
+        return 42 - sumValue;
+
+    }
+
+}
+function distanceFromHqInFeet(sumValue) {
+    if(sumValue > 42) {
+ let dif = sumValue - 42;
+
+     return dif * 264
+}
+    else {
+    let dif;
+    dif = 42 - sumValue;
+    return dif *264;
+
+}
+
+}
+function distanceTravelledInFeet(a, b) {
+    if(a < b) {
+        let dif = b - a;
+        return dif * 264;
+
+    }
+    else {
+        let dif = a - b;
+        return dif * 264;
+    }
+
+
+}
+function calculatesFarePrice(start, end) {
+let feetTravelled = distanceTravelledInFeet(start, end)
+
+   if(feetTravelled <= 400) {
+        return 0;
+
+    
+    }
+    else if (feetTravelled > 400 && feetTravelled < 2000) {
+        let price  = (feetTravelled - 400) * .02;
+    return price;
+        
+     }
+    else if(feetTravelled > 2000 && feetTravelled < 2500) {
+        return 25;
+    
+    }
+     else if (feetTravelled> 2500) {
+        return 'cannot travel that far';
+     }
+
+}
+
